2dd9234f37eb1bff9083109ca0216c60813bbbfc
K-Roman <cawme@mail.ru>
2020-06-18 18:10:10 +0300

    1й вариант

---
+++ python_snippets/зк.py
@@ -0,0 +1,78 @@
+# Сделать генератор текста на основе статистики
+# Идея проста: подсчитаем какие буквы наиболее часто стоят рядом
+# Точнее, подсчитаем как часто за буковой Х идет буква У, на основе некоего текста.
+# После этого начнем с произвольной буквы и каждую следующую будем выбирать в зависимости от
+# частоты её появления в статистике.
+
+import zipfile
+
+# zip_file_name = 'voyna-i-mir.txt.zip'
+# zfile = zipfile.ZipFile(zip_file_name, 'r')
+# for filename in zfile.namelist():
+#     zfile.extract(filename)
+from pprint import pprint
+from random import randint
+
+file_name = "voyna-i-mir.txt"
+
+stat = {}
+# stat = {'а' : {'т' : 500, 'х' : 5,}, 'т': {'о' : 100, 'у' : 50,}}
+
+analize_count = 4
+
+sequence = ' ' * analize_count
+
+with open(file_name, 'r', encoding='cp1251') as file:
+    for line in file:
+        line = line[:-1]
+        # print(line)
+        for char in line:
+            if sequence in stat:
+                if char in stat[sequence]:
+                    stat[sequence][char] += 1
+                else:
+                    stat[sequence][char] = 1
+            else:
+                stat[sequence] = {char:1}
+            sequence = sequence[1:] + char
+# pprint(stat)
+pprint(len(stat))
+
+totals = {}
+stat_for_generate = {}
+for sequence, char_stat in stat.items():
+    totals[sequence] = 0
+    stat_for_generate[sequence] = []
+    for char, count in char_stat.items():
+        totals[sequence] += count
+        stat_for_generate[sequence].append([count, char])
+    stat_for_generate[sequence].sort(reverse = True)
+
+N = 1000
+printed = 0
+sequence = ' ' * analize_count
+space_printed = 0
+while printed < N:
+    char_stat = stat_for_generate[sequence]
+    total = totals[sequence]
+    dice = randint(1, total)
+    pos = 0
+    for count, char in char_stat:
+        pos += count
+        if dice <= pos:
+            break
+    print(char, end='')
+    if char == ' ':
+        space_printed +=1
+        if space_printed >= 10:
+            print()
+            space_printed = 0
+    printed += 1
+    sequence = sequence[1:] + char
+
+
+
+
+# pprint(totals)
+# pprint(stat_for_generate)
+
